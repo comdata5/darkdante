62a292fbe633ebb020f4b30c17fb0f7e2eb14f21
comdata5 <96549099+comdata5@users.noreply.github.com>
2021-12-25 19:37:47 +0300

    Add files via upload

---
+++ modules_connect.py
@@ -0,0 +1,9 @@
+from func.parsing import *
+from func.mac_changer import *
+from func.exit import *
+from func.scan_port import *
+from func.bomber import *
+from func.blupup import *
+
+print("Библиотеки загружаются...")
+time.sleep(2)
